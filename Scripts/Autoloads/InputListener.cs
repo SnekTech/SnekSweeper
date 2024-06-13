bc105334e2b57849acc2efc4491049637fc618d7
syf <2654525303@qq.com>
2024-06-13 23:33:27 +0800

    change to main scene on restart

---
+++ Scripts/Autoloads/InputListener.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Godot;
+using SnekSweeper.Constants;
+
+namespace SnekSweeper.Autoloads;
+
+public partial class InputListener : Node
+{
+    public event Action LevelRestarted;
+
+    private const float SecondsRestartThreshold = 1;
+    private const string RestartActionName = "restart";
+
+    private bool _isRestartPressed;
+    private bool _countingRestartHold;
+    private float _restartHoldTime;
+
+    private SceneManager _sceneManager;
+
+    public override void _Ready()
+    {
+        LevelRestarted += OnLevelRestarted;
+        _sceneManager = GetNode<SceneManager>("/root/SceneManager");
+    }
+
+    public override void _ExitTree()
+    {
+        LevelRestarted -= OnLevelRestarted;
+    }
+
+    private void OnLevelRestarted()
+    {
+        GD.Print("restart");
+        _sceneManager.GotoScene(ScenePaths.MainScene);
+    }
+
+    public override void _Process(double delta)
+    {
+        ProcessRestartInput(delta);
+    }
+
+    private void ProcessRestartInput(double delta)
+    {
+        if (Input.IsActionJustPressed(RestartActionName))
+        {
+            _countingRestartHold = true;
+            _restartHoldTime = 0;
+        }
+        
+        if (_countingRestartHold && Input.IsActionPressed(RestartActionName))
+        {
+            _restartHoldTime += (float)delta;
+
+            if (_restartHoldTime > SecondsRestartThreshold)
+            {
+                LevelRestarted?.Invoke();
+            }
+        }
+
+        if (Input.IsActionJustReleased("restart"))
+        {
+            _countingRestartHold = false;
+        }
+    }
+}
--- Scripts/Constants/ScenePaths.cs
+++ Scripts/Constants/ScenePaths.cs
@@ -2,6 +2,7 @@
 
 public static class ScenePaths
 {
+    public const string MainScene = "res://Scenes/Main.tscn";
     public const string LoadingScene = "res://Scenes/Loading.tscn";
     public const string Level1 = "res://Scenes/Level1.tscn";
 }
