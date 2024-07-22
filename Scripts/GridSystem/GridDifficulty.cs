9c1ade7fe81a75af5d8a236108d36f8a7c69d482
syf <2654525303@qq.com>
2024-07-22 17:23:37 +0800

    add grid difficulty resource

---
+++ Scripts/GridSystem/GridDifficulty.cs
@@ -0,0 +1,19 @@
+﻿using Godot;
+
+namespace SnekSweeper.GridSystem;
+
+[GlobalClass]
+public partial class GridDifficulty : Resource, IGridDifficulty
+{
+    [Export(PropertyHint.Range, "5,20,")]
+    private int _rows = 10;
+
+    [Export(PropertyHint.Range, "5,30,")]
+    private int _columns = 10;
+
+    [Export(PropertyHint.Range, "0,100,10")]
+    private int _bombPercentInt = 10;
+
+    public (int rows, int columns) Size => (_rows, _columns);
+    public float BombPercent => _bombPercentInt / 100f;
+}
---
+++ Scripts/GridSystem/IGridDifficulty.cs
@@ -0,0 +1,7 @@
+﻿namespace SnekSweeper.GridSystem;
+
+public interface IGridDifficulty
+{
+    (int rows, int columns) Size { get; }
+    float BombPercent { get; }
+}
