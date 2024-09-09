d65121bce8f6ad4ed1daf3454916b961ec6d7668
syf <2654525303@qq.com>
2024-09-09 11:02:18 +0800

    add clock tick event

--- Scripts/Widgets/TestClock.cs
+++ Scripts/Widgets/TestClock.cs
@@ -5,19 +5,27 @@ namespace SnekSweeper.Widgets;
 
 public partial class TestClock : Node2D
 {
-	[Export]
-	private Clock _clock = null!;
-	
-	[Export]
-	private Label _time = null!;
+    [Export]
+    private Clock _clock = null!;
 
-	public override void _Ready()
-	{
-		_clock.Start();
-	}
+    [Export]
+    private Label _time = null!;
 
-	public override void _Process(double delta)
-	{
-		_time.Text = _clock.TimeElapsed.ToString(CultureInfo.InvariantCulture);
-	}
+    public override void _Ready()
+    {
+        _clock.Start();
+        _time.Text = 0.ToString(CultureInfo.InvariantCulture);
+
+        _clock.Ticked += OnClockTicked;
+    }
+
+    public override void _ExitTree()
+    {
+        _clock.Ticked -= OnClockTicked;
+    }
+
+    private void OnClockTicked()
+    {
+        _time.Text = _clock.TimeElapsed.ToString(CultureInfo.InvariantCulture);
+    }
 }
