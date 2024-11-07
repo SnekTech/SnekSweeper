8a2c1f9bf7f7eccbd1c108249d1035bff29f0fe1
syf <2654525303@qq.com>
2024-11-07 11:40:44 +0800

    combo progress test scene

---
+++ Scripts/Widgets/IProgress.cs
@@ -0,0 +1,9 @@
+﻿namespace SnekSweeper.Widgets;
+
+public interface IProgress
+{
+    float Value { get; set; }
+    float MaxValue { get; set; }
+    
+    // maybe style setter
+}
--- Scripts/Widgets/TestCombo.cs
+++ Scripts/Widgets/TestCombo.cs
@@ -16,16 +16,22 @@ public partial class TestCombo : Node2D
     private const int ComboWaitTime = 3;
     private int _comboLevel;
     private bool IsCountingCombo => !_comboTimer.IsStopped();
-    
-    // private StyleBoxFlat _fillStyleBox = new();
+
+    private StyleBoxFlat _fillStyleBox = new();
+    private Color[] _fillColors = { Colors.Aqua, Colors.Azure, Colors.Crimson };
 
     public override void _Ready()
     {
-        // _fillStyleBox.BgColor = Colors.Aqua;
-        // _progressBar.AddThemeStyleboxOverride("fill", _fillStyleBox);
-        
+        _progressBar.AddThemeStyleboxOverride("fill", _fillStyleBox);
+
         _button.Pressed += HandleClick;
         _comboTimer.Timeout += HandleComboTimerTimeout;
+    }
+
+    public override void _ExitTree()
+    {
+        _button.Pressed -= HandleClick;
+        _comboTimer.Timeout -= HandleComboTimerTimeout;
     }
 
     private void HandleComboTimerTimeout()
@@ -37,15 +43,25 @@ public partial class TestCombo : Node2D
     {
         if (!IsCountingCombo)
             return;
-        
+
         _progressBar.Value = _comboTimer.TimeLeft / ComboWaitTime * _progressBar.MaxValue;
     }
 
     private void HandleClick()
     {
         _progressBar.Value = _progressBar.MaxValue;
+
+        if (IsCountingCombo)
+        {
+            _comboLevel++;
+            IterateProgressFillColor(_comboLevel);
+        }
+        
         _comboTimer.Start(ComboWaitTime);
-        _comboLevel++;
-        GD.Print(_comboLevel);
+    }
+
+    private void IterateProgressFillColor(int level)
+    {
+        _fillStyleBox.BgColor = _fillColors[level % _fillColors.Length];
     }
 }
