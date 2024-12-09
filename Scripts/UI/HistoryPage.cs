7dc711d902a70d22208b7d74f4e6d1d7fe13ac82
syf <2654525303@qq.com>
2024-12-09 22:13:50 +0800

    update history page UI
    - use CanvasLayer
    - adjust layout using containers
    - add a total record count label

---
+++ Scripts/UI/HistoryPage.cs
@@ -0,0 +1,48 @@
+﻿using System.Globalization;
+using Godot;
+using GodotUtilities;
+using SnekSweeper.Autoloads;
+using SnekSweeper.Constants;
+
+namespace SnekSweeper.UI;
+
+[Scene]
+public partial class HistoryPage : CanvasLayer
+{
+    [Node] private Label recordsCountLabel = null!;
+    [Node] private VBoxContainer recordsContainer = null!;
+    [Node] private Button backToMainButton = null!;
+
+    public override void _Notification(int what)
+    {
+        if (what == NotificationSceneInstantiated)
+        {
+            WireNodes();
+        }
+    }
+
+    public override void _Ready()
+    {
+        backToMainButton.Pressed += () => SceneManager.Instance.GotoScene(SceneName.Main);
+
+        PopulateRecords();
+    }
+
+    private void PopulateRecords()
+    {
+        var records = HouseKeeper.History.Records;
+        recordsCountLabel.Text = $"{records.Count} records in total";
+        
+        foreach (var record in records)
+        {
+            var culture = CultureInfo.CurrentCulture;
+            var (startAt, endAt, winning) =
+                (record.StartAt.ToString(culture), record.EndAt.ToString(culture), record.Winning);
+            var recordString = $"from {startAt} to {endAt}, {winning}";
+
+            var label = new Label();
+            label.Text = recordString;
+            recordsContainer.AddChild(label);
+        }
+    }
+}
