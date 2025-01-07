6fd2601bf78fc1e23df4b580f183dfcc7ac2f1d1
syf <2654525303@qq.com>
2025-01-07 16:05:34 +0800

    change flag animation use position tween

--- src/SnekSweeper/Scripts/CellSystem/Components/Flag.cs
+++ src/SnekSweeper/Scripts/CellSystem/Components/Flag.cs
@@ -1,20 +1,46 @@
-﻿using System.Threading.Tasks;
+﻿using System.Threading;
+using System.Threading.Tasks;
 using Godot;
-using SnekSweeper.Widgets;
+using GodotUtilities;
+using GTweens.Easings;
+using GTweensGodot.Extensions;
+using SnekSweeper.Constants;
 
 namespace SnekSweeper.CellSystem.Components;
 
+[Scene]
 public partial class Flag : Sprite2D, IFlag
 {
+    [Node] private Sprite2D flagSprite = null!;
+
+    private const float AnimationDuration = 0.2f;
+    private const int StartPositionY = CoreStats.CellSizePixels;
+
+    public override void _Notification(int what)
+    {
+        if (what == NotificationSceneInstantiated)
+        {
+            WireNodes();
+        }
+    }
+
+    public override void _Ready()
+    {
+        Hide();
+        flagSprite.Position = flagSprite.Position with { Y = StartPositionY };
+    }
+
     public async Task RaiseAsync()
     {
         Show();
-        await this.FadeInAsync(0.5f);
+        var tween = flagSprite.TweenPositionY(0, AnimationDuration).SetEasing(Easing.OutQuad);
+        await tween.PlayAsync(CancellationToken.None);
     }
 
     public async Task PutDownAsync()
     {
-        await this.FadeOutAsync(0.5f);
+        var tween = flagSprite.TweenPositionY(StartPositionY, AnimationDuration).SetEasing(Easing.InQuad);
+        await tween.PlayAsync(CancellationToken.None);
         Hide();
     }
 }
