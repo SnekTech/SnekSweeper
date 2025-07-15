cb23bce6e12b43b1f72799c710545bb215f92cc2
syf <2654525303@qq.com>
2025-07-15 21:00:50 +0800

    use [InputMap] instead of handwritten input action names
    - also remove the rarely used InputListener autoload, which is only for restart input

--- src/SnekSweeper/Scripts/Constants/InputActions.cs
+++ src/SnekSweeper/Scripts/Constants/InputActions.cs
@@ -1,10 +1,5 @@
-﻿using Godot;
-
+﻿
 namespace SnekSweeper.Constants;
 
-public static class InputActions
-{
-    public static readonly StringName Restart = "restart";
-    public static readonly StringName Primary = "primary";
-    public static readonly StringName Secondary = "secondary";
-}
+[InputMap]
+public static partial class InputActions;
