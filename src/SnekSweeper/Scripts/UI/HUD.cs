6555c68829ed63507087c1dd2fff3a172768dc06
syf <2654525303@qq.com>
2025-07-16 23:16:02 +0800

    refactor HUD using [SceneTree]

--- src/SnekSweeper/Scripts/UI/HUD.cs
+++ src/SnekSweeper/Scripts/UI/HUD.cs
@@ -1,50 +1,37 @@
-﻿using Godot;
-using GodotUtilities;
-using SnekSweeper.Autoloads;
+﻿using SnekSweeper.Autoloads;
 using SnekSweeper.GridSystem;
+using SnekSweeper.Widgets;
 
 namespace SnekSweeper.UI;
 
-[Scene]
-public partial class HUD : CanvasLayer
+[SceneTree]
+public partial class HUD : CanvasLayer, ISceneScript
 {
-    [Node] private Label flagCountLabel = null!;
-    [Node] private Label bombCountLabel = null!;
-    [Node] private Button undoButton = null!;
-    
     private readonly GridEventBus _gridEventBus = EventBusOwner.GridEventBus;
     private readonly HUDEventBus _hudEventBus = EventBusOwner.HUDEventBus;
-
-    public override void _Notification(int what)
-    {
-        if (what == NotificationSceneInstantiated)
-        {
-            WireNodes();
-        }
-    }
 
     public override void _EnterTree()
     {
         _gridEventBus.BombCountChanged += OnBombCountChanged;
         _gridEventBus.FlagCountChanged += OnFlagCountChanged;
-        undoButton.Pressed += OnUndoPressed;
+        UndoButton.Pressed += OnUndoPressed;
     }
 
     public override void _ExitTree()
     {
         _gridEventBus.BombCountChanged -= OnBombCountChanged;
         _gridEventBus.FlagCountChanged -= OnFlagCountChanged;
-        undoButton.Pressed -= OnUndoPressed;
+        UndoButton.Pressed -= OnUndoPressed;
     }
 
     private void OnBombCountChanged(int bombCount)
     {
-        bombCountLabel.Text = $"{bombCount} bombs";
+        BombCountLabel.Text = $"{bombCount} bombs";
     }
 
     private void OnFlagCountChanged(int flagCount)
     {
-        flagCountLabel.Text = $"{flagCount} flags";
+        FlagCountLabel.Text = $"{flagCount} flags";
     }
 
     private void OnUndoPressed()
