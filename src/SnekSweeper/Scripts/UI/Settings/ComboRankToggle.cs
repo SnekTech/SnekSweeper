7c5ccfd6f0c0f5e23ad9fffc233aac4e88439ead
syf <2654525303@qq.com>
2025-11-21 21:21:25 +0800

    extract combo rank display toggle button to its own scene

---
+++ src/SnekSweeper/Scripts/UI/Settings/ComboRankToggle.cs
@@ -0,0 +1,34 @@
+﻿using SnekSweeper.Autoloads;
+using SnekSweeper.SaveLoad;
+
+namespace SnekSweeper.UI.Settings;
+
+[SceneTree]
+public partial class ComboRankToggle : HBoxContainer
+{
+    public override void _Ready()
+    {
+        InitComboRankDisplayToggle();
+    }
+
+    public override void _EnterTree()
+    {
+        ComboRankDisplayToggle.Toggled += OnComboRankDisplayToggled;
+    }
+
+    public override void _ExitTree()
+    {
+        ComboRankDisplayToggle.Toggled -= OnComboRankDisplayToggled;
+    }
+
+    private void InitComboRankDisplayToggle()
+    {
+        ComboRankDisplayToggle.SetPressedNoSignal(HouseKeeper.MainSetting.ComboRankDisplay);
+    }
+
+    private static void OnComboRankDisplayToggled(bool toggledOn)
+    {
+        HouseKeeper.MainSetting.ComboRankDisplay = toggledOn;
+        SaveLoadEventBus.EmitSaveRequested();
+    }
+}
--- src/SnekSweeper/Scripts/UI/Settings/SettingsPage.cs
+++ src/SnekSweeper/Scripts/UI/Settings/SettingsPage.cs
@@ -1,6 +1,3 @@
-using SnekSweeper.Autoloads;
-using SnekSweeper.GameSettings;
-using SnekSweeper.SaveLoad;
 using SnekSweeper.Widgets;
 
 namespace SnekSweeper.UI.Settings;
@@ -8,35 +5,4 @@ namespace SnekSweeper.UI.Settings;
 [SceneTree]
 public partial class SettingsPage : CanvasLayer, ISceneScript
 {
-    private readonly MainSetting _mainSetting = HouseKeeper.MainSetting;
-
-    #region lifecycle
-
-    public override void _EnterTree()
-    {
-        ComboRankDisplayToggle.Pressed += OnComboRankDisplayToggled;
-    }
-
-    public override void _Ready()
-    {
-        InitComboRankDisplayToggle();
-    }
-
-    public override void _ExitTree()
-    {
-        ComboRankDisplayToggle.Pressed -= OnComboRankDisplayToggled;
-    }
-
-    #endregion
-
-    private void OnComboRankDisplayToggled()
-    {
-        _mainSetting.ComboRankDisplay = !_mainSetting.ComboRankDisplay;
-        SaveLoadEventBus.EmitSaveRequested();
-    }
-
-    private void InitComboRankDisplayToggle()
-    {
-        ComboRankDisplayToggle.SetPressedNoSignal(_mainSetting.ComboRankDisplay);
-    }
 }
