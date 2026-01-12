d3e22b3370a4d0cb74ac916f6a3cde50d9024500
syf <2654525303@qq.com>
2026-01-12 22:11:13 +0800

    add cell state tests

--- tests/BasicTests/CellTests/CellStateTests.cs
+++ tests/BasicTests/CellTests/CellStateTests.cs
@@ -10,9 +10,81 @@ public class CellStateTests
     [Test]
     public async Task is_at_covered_state_after_initAsync()
     {
-        var cell = new Cell(MockHumbleCell, GridIndex.Zero);
-        await cell.InitAsync(0);
+        var cell = await CreateAndInitCellAsync();
 
         await Assert.That(cell.IsCovered).IsTrue();
+    }
+
+    [Test]
+    public async Task is_at_revealed_state_after_revealing()
+    {
+        var cell = await CreateAndInitCellAsync();
+
+        await cell.RevealAsync();
+
+        await Assert.That(cell.IsRevealed).IsTrue();
+    }
+
+    [Test]
+    public async Task is_flagged_if_switch_flag_on_covered_cell()
+    {
+        var cell = await CreateAndInitCellAsync();
+
+        await cell.SwitchFlagAsync();
+
+        await Assert.That(cell.IsFlagged).IsTrue();
+    }
+
+    [Test]
+    public async Task is_covered_if_switch_flag_on_flagged_cell()
+    {
+        var cell = await CreateAndInitCellAsync();
+
+        await cell.SwitchFlagAsync();
+        await Assert.That(cell.IsFlagged).IsTrue();
+        await cell.SwitchFlagAsync();
+        await Assert.That(cell.IsCovered).IsTrue();
+    }
+
+    [Test]
+    public async Task is_still_revealed_if_switch_flag_on_revealed_cell()
+    {
+        var cell = await CreateAndInitCellAsync();
+
+        await cell.RevealAsync();
+        await Assert.That(cell.IsRevealed).IsTrue();
+
+        await cell.SwitchFlagAsync();
+        await Assert.That(cell.IsRevealed).IsTrue();
+    }
+
+    [Test]
+    public async Task is_still_revealed_if_reveal_on_revealed_cell()
+    {
+        var cell = await CreateAndInitCellAsync();
+
+        await cell.RevealAsync();
+        await Assert.That(cell.IsRevealed).IsTrue();
+        
+        await cell.RevealAsync();
+        await Assert.That(cell.IsRevealed).IsTrue();
+    }
+
+    [Test]
+    public async Task is_still_flagged_if_reveal_on_flagged_cell()
+    {
+        var cell = await CreateAndInitCellAsync();
+
+        await cell.SwitchFlagAsync();
+        await Assert.That(cell.IsFlagged).IsTrue();
+        await cell.RevealAsync();
+        await Assert.That(cell.IsFlagged).IsTrue();
+    }
+
+    static async Task<Cell> CreateAndInitCellAsync()
+    {
+        var cell = new Cell(MockHumbleCell, GridIndex.Zero);
+        await cell.InitAsync(0);
+        return cell;
     }
 }
