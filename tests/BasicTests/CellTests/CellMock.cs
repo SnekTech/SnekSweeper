34c06d9f21443c559d4f22b314834118ac085a39
syf <2654525303@qq.com>
2026-01-12 18:41:06 +0800

    add cell state test

---
+++ tests/BasicTests/CellTests/CellMock.cs
@@ -0,0 +1,51 @@
+﻿using Dumpify;
+using SnekSweeperCore.CellSystem;
+using SnekSweeperCore.CellSystem.Components;
+
+namespace BasicTests.CellTests;
+
+class MockHumbleCell : IHumbleCell
+{
+    public ICover Cover { get; } = new MockCover();
+    public IFlag Flag { get; } = new MockFlag();
+    public void SetContent(bool hasBomb, int neighborBombCount)
+    {
+        var initInfo = $"hasBomb = {hasBomb}, neighbor bomb count = {neighborBombCount}";
+        $"setting cell content with {initInfo}".Dump();
+    }
+}
+
+class MockCover :ICover
+{
+    public Task RevealAsync(CancellationToken cancellationToken)
+    {
+        "revealing the cover".Dump();
+        return Task.CompletedTask;
+    }
+
+    public Task PutOnAsync(CancellationToken cancellationToken)
+    {
+        "putting on the cover".Dump();
+        return Task.CompletedTask;
+    }
+
+    public void SetAlpha(float normalizedAlpha)
+    {
+        $"setting cover alpha to {normalizedAlpha}".Dump();
+    }
+}
+
+class MockFlag : IFlag
+{
+    public Task RaiseAsync(CancellationToken cancellationToken)
+    {
+        "raising flag".Dump();
+        return Task.CompletedTask;
+    }
+
+    public Task PutDownAsync(CancellationToken cancellationToken)
+    {
+        "putting down flag".Dump();
+        return Task.CompletedTask;
+    }
+}
---
+++ tests/BasicTests/CellTests/CellStateTests.cs
@@ -0,0 +1,18 @@
+﻿using SnekSweeperCore.CellSystem;
+using SnekSweeperCore.GridSystem;
+
+namespace BasicTests.CellTests;
+
+public class CellStateTests
+{
+    static readonly MockHumbleCell MockHumbleCell = new();
+
+    [Test]
+    public async Task is_at_covered_state_after_initAsync()
+    {
+        var cell = new Cell(MockHumbleCell, GridIndex.Zero);
+        await cell.InitAsync(0);
+
+        await Assert.That(cell.IsCovered).IsTrue();
+    }
+}
