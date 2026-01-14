b7e006f527f5b8bd2d62d159d8efe4a2903405ba
syf <2654525303@qq.com>
2026-01-14 21:56:00 +0800

    matrix converter TDD

---
+++ tests/BasicTests/GridTests/MatrixConverter.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+
+namespace BasicTests.GridTests;
+
+static class MatrixConverter
+{
+    // public bool[,]? Read()
+    // {
+    //     var mat = DefaultStringListConverter.Read(ref reader, typeof(List<string>), options);
+    //     if (mat == null)
+    //         return null;
+    //
+    //     var rows = mat.Count;
+    //     if (rows <= 0)
+    //         throw new InvalidOperationException("matrix has <= 0 rows");
+    //     var columns = mat[0].Length;
+    //     if (columns <= 0)
+    //         throw new InvalidOperationException("matrix has <= 0 columns");
+    //
+    //     var result = new bool[rows, columns];
+    //     for (var i = 0; i < rows; i++)
+    //     {
+    //         for (var j = 0; j < columns; j++)
+    //         {
+    //             result[i, j] = mat[i][j] != '0';
+    //         }
+    //     }
+    //
+    //     return result;
+    // }
+
+    public static List<string> ToMatrixInList(bool[,] matrix)
+    {
+        var rowList = new List<string>();
+        var (rows, columns) = (matrix.GetLength(0), matrix.GetLength(1));
+        var sb = new StringBuilder();
+        for (var i = 0; i < rows; i++)
+        {
+            sb.Clear();
+            for (var j = 0; j < columns; j++)
+            {
+                sb.Append(matrix[i, j] switch
+                {
+                    true => '1',
+                    false => '0',
+                });
+            }
+            rowList.Add(sb.ToString());
+        }
+
+        return rowList;
+    }
+}
---
+++ tests/BasicTests/GridTests/MatrixConverterSpecs.cs
@@ -0,0 +1,46 @@
+﻿namespace BasicTests.GridTests;
+
+public class MatrixConverterSpecs
+{
+    [Test]
+    public async Task from_matrix_to_list_of_string()
+    {
+        // todo: data-driven test
+        var (matrix, expectedList) = Examples[1];
+
+        var list = MatrixConverter.ToMatrixInList(matrix);
+
+        await Assert.That(list).IsEquivalentTo(expectedList);
+    }
+
+    static readonly MatrixListPair[] Examples =
+    [
+        new(new[,]
+            {
+                { false, false, false },
+                { false, true, false },
+                { false, false, false },
+            },
+            [
+                "000",
+                "010",
+                "000",
+            ]),
+        
+        new(new[,]
+            {
+                { false, false, false },
+                { false, true, false },
+                { false, true, true },
+            },
+            [
+                "000",
+                "010",
+                "011",
+            ]),
+
+        new(new bool[,] { }, []),
+    ];
+}
+
+readonly record struct MatrixListPair(bool[,] Matrix, List<string> MatrixInList);
