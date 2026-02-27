14472014caae24573b5261faa3df7ab4729b6292
syf <2654525303@qq.com>
2026-02-27 17:56:05 +0800

    create "press to start" label

---
+++ src/SnekSweeper/Scripts/UI/MainScreen/PressToStartLabel.cs
@@ -0,0 +1,32 @@
+﻿using GTweens.Easings;
+using GTweens.Enums;
+using GTweens.Tweens;
+using GTweensGodot.Extensions;
+
+namespace SnekSweeper.UI.MainScreen;
+
+public partial class PressToStartLabel : Label
+{
+    GTween? _blinkTween;
+    const float BlinkDuration = 0.6f;
+
+    public override void _Ready()
+    {
+        StartBlink();
+    }
+
+    public override void _ExitTree()
+    {
+        _blinkTween?.Kill();
+    }
+
+    void StartBlink()
+    {
+        if (_blinkTween is not null) return;
+
+        _blinkTween = this.TweenModulateAlpha(0, BlinkDuration)
+            .SetEasing(Easing.InOutSine)
+            .SetMaxLoops(ResetMode.PingPong);
+        _blinkTween.Play();
+    }
+}
