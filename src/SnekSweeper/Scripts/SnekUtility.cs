c3cd6b47a98425557b4b269a45c6ed9337a6e8d5
syf <2654525303@qq.com>
2026-04-10 22:01:59 +0800

    remove unused utilities

--- src/SnekSweeper/Scripts/SnekUtility.cs
+++ src/SnekSweeper/Scripts/SnekUtility.cs
@@ -1,16 +1,6 @@
-﻿using GTweens.Builders;
-using GTweensGodot.Extensions;
-
-namespace SnekSweeper;
+﻿namespace SnekSweeper;
 
 public static class SnekUtility
 {
-    public static Task DelayGd(float timeSec, CancellationToken cancellationToken = default)
-    {
-        return GTweenSequenceBuilder.New().AppendTime(timeSec).Build().PlayAsync(cancellationToken);
-    }
-
-    public static PackedScene LoadScene(string path) => ResourceLoader.Load<PackedScene>(path);
     public static Texture2D LoadTexture(string path) => ResourceLoader.Load<Texture2D>(path);
-    public static AudioStream LoadSound(string path) => ResourceLoader.Load<AudioStream>(path);
 }
